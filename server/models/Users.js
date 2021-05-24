85bdb8c63baa552d4342b7c9f2b2b3d1727af3e6
ClaudiuRaveanu <claudiu.raveanu00@e-uvt.ro>
2021-05-24 22:20:03 +0300

    few bugs fixed, reviews 99% working (no user_id)

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,15 +1,31 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: {
+    name: {
+        type: String,
+        required: true
+    },
+    password: { type: String, required: true },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+        required: true
+    },
+    email: {
         type: String,
         unique: true,
         required: true
     },
-    password: {type: String, required: true},
-    isAdmin: {
-        type: Boolean,
-        default: false,
+    surname: {
+        type: String,
+        required: true
+    },
+    id_nr: {
+        type: String,
+        required: true
+    },
+    phone_nr: {
+        type: String,
         required: true
     }
 });
